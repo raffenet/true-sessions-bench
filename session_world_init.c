616be95388fa307f68bee6bc99fa8c9b74b8ebfc
Ken Raffenetti <raffenet@mcs.anl.gov>
2025-05-09 12:57:48 -0500

    same changes to world init to session world init

--- session_world_init.c
+++ session_world_init.c
@@ -8,55 +8,50 @@ void pairwise_exchange(int rank, int size, MPI_Comm comm);
 
 int main(void)
 {
-  struct timespec start, end;
-  double my_time, total_time;
-  MPI_Session session;
-  MPI_Group group;
-  MPI_Comm comm_world;
-  int rank, size;
+    struct timespec start, init_end,
+        ex_start, ex1_end, ex2_end;
+    double times[3], total_times[3];
+    MPI_Session session;
+    MPI_Group group;
+    MPI_Comm comm_world;
+    int rank, size;
 
-  clock_gettime(CLOCK_MONOTONIC, &start);
-  MPI_Session_init(MPI_INFO_NULL, MPI_ERRORS_ABORT, &session);
-  MPI_Group_from_session_pset(session, "mpi://WORLD", &group);
-  MPI_Comm_create_from_group(group, "comm_world", MPI_INFO_NULL, MPI_ERRORS_ABORT, &comm_world);
-  MPI_Comm_rank(comm_world, &rank);
-  MPI_Comm_size(comm_world, &size);
-  pairwise_exchange(rank, size, comm_world);
-  clock_gettime(CLOCK_MONOTONIC, &end);
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    MPI_Session_init(MPI_INFO_NULL, MPI_ERRORS_ABORT, &session);
+    MPI_Group_from_session_pset(session, "mpi://WORLD", &group);
+    MPI_Comm_create_from_group(group, "comm_world", MPI_INFO_NULL, MPI_ERRORS_ABORT, &comm_world);
+    clock_gettime(CLOCK_MONOTONIC, &init_end);
 
-  my_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-  MPI_Reduce(&my_time, &total_time, 1, MPI_DOUBLE, MPI_SUM, 0, comm_world);
-  if (rank == 0) {
-    printf("session world init %f, size %d\n", total_time / size, size);
-  }
+    MPI_Comm_rank(comm_world, &rank);
+    MPI_Comm_size(comm_world, &size);
 
-  MPI_Comm newcomm;
-  MPI_Barrier(comm_world);
-  clock_gettime(CLOCK_MONOTONIC, &start);
-  MPI_Comm_dup(comm_world, &newcomm);
-  clock_gettime(CLOCK_MONOTONIC, &end);
-  MPI_Comm_free(&newcomm);
+    clock_gettime(CLOCK_MONOTONIC, &ex_start);
+    pairwise_exchange(rank, size, comm_world);
+    clock_gettime(CLOCK_MONOTONIC, &ex1_end);
+    pairwise_exchange(rank, size, comm_world);
+    clock_gettime(CLOCK_MONOTONIC, &ex2_end);
 
-  my_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-  MPI_Reduce(&my_time, &total_time, 1, MPI_DOUBLE, MPI_SUM, 0, comm_world);
-  if (rank == 0) {
-    printf("session world comm dup %f\n", total_time / size);
-  }
+    times[0] = (init_end.tv_sec - start.tv_sec) + (init_end.tv_nsec - start.tv_nsec) / 1000000000.0;
+    times[1] = (ex1_end.tv_sec - ex_start.tv_sec) + (ex1_end.tv_nsec - ex_start.tv_nsec) / 1000000000.0;
+    times[2] = (ex2_end.tv_sec - ex1_end.tv_sec) + (ex2_end.tv_nsec - ex1_end.tv_nsec) / 1000000000.0;
+    MPI_Reduce(times, total_times, 3, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        printf("session world init %f, pairwise1 %f, pairwise2 %f\n", total_times[0] / size, total_times[1] / size, total_times[2] / size);
+    }
 
-  MPI_Comm_free(&comm_world);
-  MPI_Group_free(&group);
-  MPI_Session_finalize(&session);
+    MPI_Comm_free(&comm_world);
+    MPI_Group_free(&group);
+    MPI_Session_finalize(&session);
 
-  return 0;
+    return 0;
 }
 
 void pairwise_exchange(int rank, int size, MPI_Comm comm)
 {
-  int dummy;
-  for (int step = 1; step < size; ++step) {
-    int send_to = (rank + step) % size;
-    int recv_from = (rank - step + size) % size;
-    MPI_Sendrecv(&dummy, 0, MPI_BYTE, send_to, 0, &dummy, 0, MPI_BYTE, recv_from, 0,
-                 comm, MPI_STATUS_IGNORE);
-  }
+    for (int step = 1; step < size; ++step) {
+        int send_to = (rank + step) % size;
+        int recv_from = (rank - step + size) % size;
+        MPI_Sendrecv(NULL, 0, MPI_BYTE, send_to, 0, NULL, 0, MPI_BYTE, recv_from, 0,
+                     comm, MPI_STATUS_IGNORE);
+    }
 }
