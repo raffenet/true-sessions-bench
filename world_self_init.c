40d2e54fe45b9a3ca334fd18fea377fad818f2f6
Ken Raffenetti <raffenet@mcs.anl.gov>
2025-05-15 14:56:16 -0500

    add WPM version of self init test

---
+++ world_self_init.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <time.h>
+#include <mpi.h>
+
+int main(void)
+{
+  struct timespec start, end;
+  double my_time;
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  MPI_Init(NULL, NULL);
+  /* send to self to verify communication is functional */
+  MPI_Sendrecv(NULL, 0, MPI_INT, 0, 0, NULL, 0, MPI_INT, 0, 0, MPI_COMM_SELF, MPI_STATUS_IGNORE);
+  clock_gettime(CLOCK_MONOTONIC, &end);
+
+  my_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank == 0) {
+      printf("self init %f\n", my_time);
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
