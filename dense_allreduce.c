e7c7e179d0aa84f4b86d6332abfa94b664578a56
Ken Raffenetti <raffenet@mcs.anl.gov>
2025-05-19 21:00:08 -0500

    fix timing in dense_allreduce

--- dense_allreduce.c
+++ dense_allreduce.c
@@ -5,9 +5,8 @@
 
 int main(int argc, char *argv[])
 {
-    struct timespec start, init_end,
-        ex_start, ex1_end, ex2_end;
-    double times[3], total_times[3];
+    struct timespec allred_start, allred_end;
+    double time, total_time;
 
     MPI_Session session;
     MPI_Group group;
@@ -21,17 +20,16 @@ int main(int argc, char *argv[])
     MPI_Comm_create_from_group(group, "comm_world", MPI_INFO_NULL, MPI_ERRORS_ABORT, &comm_world);
 
     int a=1, b;
+    clock_gettime(CLOCK_MONOTONIC, &allred_start);
     MPI_Allreduce(&a, &b, 1, MPI_INT, MPI_SUM, comm_world);
+    clock_gettime(CLOCK_MONOTONIC, &allred_end);
 
-    times[0] = (init_end.tv_sec - start.tv_sec) + (init_end.tv_nsec - start.tv_nsec) / 1000000000.0;
-    times[1] = (ex1_end.tv_sec - ex_start.tv_sec) + (ex1_end.tv_nsec - ex_start.tv_nsec) / 1000000000.0;
-    times[2] = (ex2_end.tv_sec - ex1_end.tv_sec) + (ex2_end.tv_nsec - ex1_end.tv_nsec) / 1000000000.0;
-    MPI_Reduce(times, total_times, 3, MPI_DOUBLE, MPI_SUM, 0, comm_world);
+    times[0] = (allred_end.tv_sec - allred_start.tv_sec) + (allred_end.tv_nsec - allred_start.tv_nsec) / 1000000000.0;
+    MPI_Reduce(&time, &total_time, 1, MPI_DOUBLE, MPI_SUM, 0, comm_world);
     MPI_Comm_rank(comm_world, &world_rank);
     MPI_Comm_size(comm_world, &world_size);
     if (world_rank == 0) {
-        printf("world init %f, pairwise1 %f, pairwise2 %f\n", total_times[0] / world_size,
-               total_times[1] / world_size, total_times[2] / world_size);
+        printf("dense allreduce %f\n", total_time / world_size);
     }
 
     MPI_Group_free(&group);
